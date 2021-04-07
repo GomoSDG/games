6216de5c24dcc8c2c565dd1e22320e6af1e5579a
simply-gomotso <gomotso@simply.co.za>
2021-04-07 18:05:51 +0200

    add comments about ideas on what to do next.

--- src/ui/gomosdg/client/core.cljs
+++ src/ui/gomosdg/client/core.cljs
@@ -16,8 +16,9 @@
   conn
   (.send conn (.stringify js/JSON (js-obj "Hello" "world"))))
 
-
-;; We to make sure that messages are received by the right component.
-;; No users have an id in the beginning. Something that can be simplified by cognito, but no time.
-;; Consider making the rooms reusable. Not really what I want.
+;; TODO: Things that need to get done before we can start doing the UI:
+;; * Send back acknowledgements. Might not be that necessary. ????
+;; * Send back who is the winner.
+;; * Handle leaving the room and sending people out.
+;; * Move code into the right files. Determine suitable file structure.
 
