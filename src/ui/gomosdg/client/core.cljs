f711cba54efc2da3d2d04dde7a471945ba653205
simply-gomotso <gomotso@simply.co.za>
2021-04-08 21:43:32 +0200

    update cljs code to test game

--- src/ui/gomosdg/client/core.cljs
+++ src/ui/gomosdg/client/core.cljs
@@ -1,18 +1,32 @@
 (ns gomosdg.client.core)
 
+(defn log-last-message [k t o n]
+  (js/console.log "New Message: " (.parse js/JSON (last n))))
+
+(def p1-msge (atom []))
+(def p2-msge (atom []))
+
+(add-watch p1-msge :watcher log-last-message)
+(add-watch p2-msge :watcher log-last-message)
+
 (comment
+  @p1-msge
   (def p1 (js/WebSocket. "ws://localhost:8080/tic-tac-toe"))
   (.send p1 (.stringify js/JSON (js-obj "type" "place-symbol"
-                                        "id" "1"
+                                        "id" (.-id (.parse js/JSON (first @p1-msge)))
                                         "symbol" "o"
                                         "pos" 2)))
   (set! (.-onmessage p1)
         (fn [e]
-          (js/console.log "Message for p1: " (.-data e))))
+          (swap! p1-msge conj (.-data e))))
   (def p2 (js/WebSocket. "ws://localhost:8080/tic-tac-toe"))
-  (set! (.-onmessage p1)
+  (set! (.-onmessage p2)
         (fn [e]
-          (js/console.log "Message for p2: " (.-data e))))
+          (swap! p2-msge conj (.-data e))))
+  (.send p2 (.stringify js/JSON (js-obj "type" "place-symbol"
+                                        "id" (.-id (.parse js/JSON (first @p2-msge)))
+                                        "symbol" "o"
+                                        "pos" 2)))
   conn
   (.send conn (.stringify js/JSON (js-obj "Hello" "world"))))
 
