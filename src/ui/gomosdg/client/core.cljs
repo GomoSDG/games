d4c013faf6e20b10c42c604f446df11c410ffa75
simply-gomotso <gomotso@simply.co.za>
2021-04-09 07:47:15 +0200

    remove logging and add code to help test server.

--- src/ui/gomosdg/client/core.cljs
+++ src/ui/gomosdg/client/core.cljs
@@ -11,11 +11,12 @@
 
 (comment
   @p1-msge
+  @p2-msge
   (def p1 (js/WebSocket. "ws://localhost:8080/tic-tac-toe"))
   (.send p1 (.stringify js/JSON (js-obj "type" "place-symbol"
                                         "id" (.-id (.parse js/JSON (first @p1-msge)))
                                         "symbol" "o"
-                                        "pos" 2)))
+                                        "pos" 7)))
   (set! (.-onmessage p1)
         (fn [e]
           (swap! p1-msge conj (.-data e))))
@@ -26,13 +27,5 @@
   (.send p2 (.stringify js/JSON (js-obj "type" "place-symbol"
                                         "id" (.-id (.parse js/JSON (first @p2-msge)))
                                         "symbol" "o"
-                                        "pos" 2)))
-  conn
-  (.send conn (.stringify js/JSON (js-obj "Hello" "world"))))
-
-;; TODO: Things that need to get done before we can start doing the UI:
-;; * Send back acknowledgements. Might not be that necessary. ????
-;; * Send back who is the winner.
-;; * Handle leaving the room and sending people out.
-;; * Move code into the right files. Determine suitable file structure.
-
+                                        "pos" 0)))
+  )
