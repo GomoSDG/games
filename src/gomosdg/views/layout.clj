d813c23b1c197c6985d59b38c35cb5bbccc310e0
simply-gomotso <gomotso@simply.co.za>
2021-05-09 12:52:27 +0200

    make bulma responsive

--- src/gomosdg/views/layout.clj
+++ src/gomosdg/views/layout.clj
@@ -5,6 +5,9 @@
   (html5
     [:head
      [:title title]
+     [:meta {:charsets "utf-8"}]
+     [:meta {:name "viewport"
+             :content "width=device-width, initial-scale=1"}]
      (include-js "/js/turbo.es5-umd.js")
      (include-css "https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css")]
     [:body
